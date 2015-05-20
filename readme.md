7c6e425f823970a414ae4260fe00e119e139591e
Sachi <sshirasaka@deloitte.com>
2015-05-19 20:58:46 -0500

    fixed issue with routing, added sass support, started restructuring code directory

--- readme.md
+++ readme.md
@@ -1,11 +1,20 @@
-React Kickstarter for projects
-==============================
+# React Kickstarter for projects
 
-Components to include:
+## Components included (05/19/2015):
 - Bootstrap components, native to react so they can be replaced/extended as needed
-- React router
-- Basic react projec structure, with sample pages
+- React router and react router bootstrap
+- Basic react project structure, with sample pages
+  - css folder for css pages. We are adding bootstrap.min.css as a starter point
+
 - Webpack for bundling
+- Babel.js, because we want to learn EcmaScript6 right?
 
+## Running the Project
+### Before running:
+* npm install
 
-- ES6, cause we want to learn the latest stuff coming to the browsers
+### To build the project:
+* npm run build
+
+### To run the dev environment using Webpack:
+* npm start
--- webpack.config.js
+++ webpack.config.js
@@ -1,22 +1,63 @@
 var webpack = require('webpack');
+var path = require("path");
+
 module.exports = {
+    cache: true,
+    context: __dirname,
+    //the entry section tells webpack where the entry file is,
+    //and we have the development server for webpack and react-hot-loader
     entry: [
       'webpack/hot/only-dev-server',
-      "./js/app.js"
+      "./app/app.js"
     ],
+    //The output section is where the bundled files should go when we build our app.
+    //In this case, the files will be in /build/bundle.js
     output: {
-        path: __dirname + '/build',
-        filename: "bundle.js"
+        path: path.join(__dirname,'./build'),
+        filename: "[name].js",
+        publicPath: "/"
     },
+    resolve: {
+      extensions: ["", ".js", ".css", ".sass", ".jpg", ".png", ".svg", ".gif"],
+      modulesDirectories: ["app", "node_modules"]
+    },
+    //This is where you add/remove loaders based on what you need webpack to bundle for you.
+    //The two main ones we are using are react-hot and babel.
     module: {
         loaders: [
-            { test: /\.js?$/, loaders: ['react-hot', 'babel'], exclude: /node_modules/ },
-            { test: /\.js$/, exclude: /node_modules/, loader: 'babel-loader'},
-            { test: /\.css$/, loader: "style!css" }
+            {
+              test: /\.js?$/,
+              loaders: ['react-hot', 'babel'],
+              exclude: /node_modules/
+            },
+            {
+              test: /\.js$/,
+              exclude: /node_modules/,
+              loader: 'babel-loader'
+            },
+            {
+              test: /\.sass$/,
+              loaders: [
+                'style-loader',
+                'css-loader',
+                'autoprefixer-loader?browsers=last 2 version',
+                'sass-loader?indentedSyntax=sass&includePaths[]=' + path.resolve(__dirname, "./app")
+              ],
+            },
+            {
+              test: /\.(jpg|png|svg|gif|eot|ttf|woff)$/,
+              loader: "url-loader",
+              query: {
+                name: "[path][name].[ext]",
+                context: "app",
+                limit: "8192",
+              }
+            }
         ]
     },
+    //The last section is plugins and include different type of webpack plugins.
     plugins: [
-      new webpack.NoErrorsPlugin()
+      new webpack.HotModuleReplacementPlugin()
     ]
 
 };
